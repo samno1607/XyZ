f1b02d287dd1756807f2e3195d728af02ec1fac4
samno1607 <swig.sg@googlemail.com>
2013-03-31 19:18:36 +0100

    Add UV Interface From Faux123 With Modified VDD Limits

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 31 02:45:01 BST 2013"
+#define LINUX_COMPILE_BY "samno"
+#define LINUX_COMPILE_HOST "Penguin-1"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-g5159425-dirty"
