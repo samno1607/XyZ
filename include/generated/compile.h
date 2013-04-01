7607732925ebdc9ae283906e2b2c71efbcf248ba
samno1607 <swig.sg@googlemail.com>
2013-04-01 02:28:36 +0100

    Added Some IO Schedulers

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Mar 31 20:04:29 BST 2013"
+#define UTS_VERSION "#5 SMP PREEMPT Mon Apr 1 02:24:49 BST 2013"
 #define LINUX_COMPILE_BY "samno"
 #define LINUX_COMPILE_HOST "Penguin-1"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-gb2e35de-dirty"
+#define UTS_RELEASE "3.4.0-g83c5b45-dirty"
